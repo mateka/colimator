d24236dbc8c1ec15d1498b3c892370c7cb6913dc
Mateusz Adamczyk <matek_a@outlook.com>
2021-10-30 15:51:45 +0200

    Initial Commit

---
+++ colimator/colimatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  colimatorApp.swift
+//  colimator
+//
+//  Created by Mateusz Adamczyk on 30/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct colimatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
